053cccee6a607475dc1e48795b93c326f4417eba
sujalbahing <sujal.rai147@gmail.com>
2024-07-27 17:23:30 +0545

    home and order page added

--- Dgarden/Dgarden/settings.py
+++ Dgarden/Dgarden/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'Dgarden.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'static/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
