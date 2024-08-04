38ef7c8362dea37598be838ed706239eaed75c29
sujalbahing <sujal.rai147@gmail.com>
2024-08-05 00:01:34 +0545

    Clean Website Done

--- Dgarden/Dgarden/settings.py
+++ Dgarden/Dgarden/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     
     # Local Apps
     'pizza',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -117,12 +118,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+import os
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static',
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
